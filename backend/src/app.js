32db24af60c4ca49f00039185c63f6a85aba772a
bachewar-bhushan <150711051+bachewar-bhushan@users.noreply.github.com>
2025-12-29 17:22:11 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 // src/app.js
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
 const app = express();
 
 // middleware
 app.use(express.json());
-
+app.use(cookieParser());
 // test route
 app.get("/", (req, res) => {
   res.send("API running");
