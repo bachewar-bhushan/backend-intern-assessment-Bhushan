325cf09ae64292405d8c117bef73682231de4a60
bachewar-bhushan <150711051+bachewar-bhushan@users.noreply.github.com>
2025-12-31 00:07:46 +0530

    updated cookie for production in controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -43,8 +43,8 @@ exports.signup = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, 
-      sameSite: "strict",
+      secure: true, 
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
